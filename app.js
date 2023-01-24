2c597ea5a8db496811e7fc7d93efd88f4c422237
Aram Hekimian <aramhek121@gmail.com>
2023-01-24 12:11:33 -0600

    Added game vcomponents, missing game logic

---
+++ app.js
@@ -0,0 +1,62 @@
+const domElems = (() => {
+  const board = document.getElementById("main-cont");
+  const squares = document.getElementsByClassName("game-square");
+  const markers = document.getElementsByClassName("marker");
+  const btnStart = document.getElementById("btn-start");
+  const form = document.getElementById("game-start");
+  const inputName = document.getElementById("input-name");
+  const inputMarkerX = document.getElementById("input-markerX");
+  const inputMarkerO = document.getElementById("input-markerO");
+  return {
+    board,
+    squares,
+    markers,
+    btnStart,
+    inputName,
+    inputMarkerO,
+    inputMarkerX,
+    form,
+  };
+})();
+
+const addEventToNodes = (evn, nodelst, func) => {
+  for (let i = 0; i < nodelst.length; i++) {
+    nodelst[i].addEventListener(evn, func);
+  }
+};
+
+const gameBoard = (() => [" ", " ", " ", " ", " ", " ", " ", " ", " "])();
+const displayBoard = () => {
+  for (let i = 0; i < gameBoard.length; i++) {
+    domElems.markers[i].innerText = gameBoard[i];
+  }
+};
+displayBoard();
+
+const Player = (name, marker) => ({ name, marker });
+
+const game = (() => {
+  let player1;
+  let player2;
+  domElems.btnStart.addEventListener("click", () => {
+    if (domElems.inputName.value !== "") {
+      domElems.board.style.display = "grid";
+      domElems.form.style.display = "none";
+      if (domElems.inputMarkerO.checked) {
+        player1 = Player(domElems.inputName.value, "O");
+        player2 = Player("Computer", "X");
+      } else if (domElems.inputMarkerX.checked) {
+        player1 = Player(domElems.inputName.value, "X");
+        player2 = Player("Computer", "O");
+      }
+    }
+    console.log("player1", player1);
+    console.log("player2", player2);
+  });
+
+  const squaresArr = Array.prototype.slice.call(domElems.squares);
+  addEventToNodes("click", domElems.squares, function () {
+    gameBoard[squaresArr.indexOf(this)] = player1.marker;
+    displayBoard();
+  });
+})();
